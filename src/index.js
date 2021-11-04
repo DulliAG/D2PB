f3fde8db70ff9f64e6b0f1799908720d6c4dc840
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-11-04 11:18:04 +0100

    save

--- src/index.js
+++ src/index.js
@@ -55,145 +55,152 @@ client.on('ready', () => {
       const items = await dota.getItemList();
       const heroes = await dota.getHeroList();
 
-      dota.getLatestPatchNote().then((pnote) => {
-        const { patch } = require('./latest_patch.json');
-        if (patch == pnote.patch_name) {
-          helper.log('No new Dota patch avaiable!');
-          return;
-        }
+      fs.readFile('./src/latest_patch.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        const latestPatchFile = JSON.parse(data),
+          patch = latestPatchFile.patch;
 
-        helper.log(`New Dota patch ${pnote.patch_name} is avaiable!`);
-
-        client.guilds.cache.forEach((guild) => {
-          const role = guild.roles.cache.find(
-            (r) =>
-              r.name.toLowerCase().includes(message.role_name.toLocaleLowerCase()) &&
-              !r.name.includes('Dota2 Patches')
-          );
-          let msg = role ? `${role}` : '';
-          Object.keys(pnote)
-            .filter((key) => {
-              return (
-                key !== 'patch_number' &&
-                key !== 'patch_name' &&
-                key !== 'patch_timestamp' &&
-                key !== 'success'
-              );
-            })
-            .forEach((category) => {
-              const changes = pnote[category];
-              const categoryName = category.charAt(0).toUpperCase() + category.slice(1);
-              msg += `\n**${categoryName}**\n`;
-              switch (category) {
-                case 'generic':
-                  changes.forEach((change) => (msg += `- ${change.note}\n`));
-                  break;
-
-                case 'items':
-                  changes.forEach((change) => {
-                    const id = change.ability_id;
-                    const item_name = items.find((i) => i.id == id).name_english_loc;
-                    const notes = change.ability_notes;
-                    msg += `**${item_name}**\n`;
-                    notes.forEach((note) => (msg += `- ${note.note}\n`));
-                  });
-                  break;
-
-                case 'heroes':
-                  changes.forEach((change) => {
-                    const hero_id = change.hero_id;
-                    const hero_name = heroes.find((h) => h.id == hero_id).name_english_loc;
-                    const hero_notes = change.hero_notes;
-                    const talent_notes = change.talent_notes;
-                    const abilities = change.abilities;
-                    msg += `**${hero_name}**\n`;
-
-                    if (hero_notes) {
-                      hero_notes.forEach((i) => (msg += `- ${i.note}\n`));
-                    }
-
-                    if (talent_notes) {
-                      talent_notes.forEach((i) => (msg += `- ${i.note}\n`));
-                    }
-
-                    if (abilities) {
-                      abilities.forEach((i) =>
-                        i.ability_notes.forEach((note) => (msg += `- ${note.note}\n`))
-                      );
-                    }
-                  });
-                  break;
-
-                case 'neutral_items':
-                  changes.forEach((change) => {
-                    const id = change.ability_id;
-                    const item_name = items.find((i) => i.id == id).name_english_loc;
-                    const notes = change.ability_notes;
-                    msg += `**${item_name}**\n`;
-                    notes.forEach((note) => (msg += `- ${note.note}\n`));
-                  });
-                  break;
-              }
-            });
-
-          const embed = {
-            color: 0x0099ff,
-            title: `Patch ${pnote.patch_name}`,
-            url: `https://www.dota2.com/patches/${pnote.patch_name}`,
-            description: 'There is a new patch!',
-            thumbnail: {
-              url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_symbol.png',
-            },
-            fields: [
-              {
-                name: 'Patch',
-                value: pnote.patch_name,
-                inline: true,
-              },
-              {
-                name: 'Patch notes',
-                value: `[Here](https://www.dota2.com/patches/${pnote.patch_name})`,
-                inline: true,
-              },
-            ],
-          };
-
-          // Send update notification
-          const updateChannel = guild.channels.cache.filter(
-            (c) =>
-              c.isText &&
-              c.name.toLowerCase().includes(message.channel_update_name.toLocaleLowerCase())
-          );
-          if (updateChannel) {
-            updateChannel.forEach((ch) => {
-              role
-                ? ch.send({ content: `${role}`, embeds: [embed] }).catch((err) => helper.error(err))
-                : ch.send({ embeds: [embed] }).catch((err) => helper.error(err));
-            });
+        dota.getLatestPatchNote().then(async (pnote) => {
+          if (patch == pnote.patch_name) {
+            helper.log('No new Dota patch avaiable!');
+            return;
           }
 
-          // Send changelogs
-          Discord.Util.splitMessage(msg, { maxLength: 2000 }).forEach((splittedMessage) => {
-            const changelogChannels = guild.channels.cache.filter(
+          helper.log(`New Dota patch ${pnote.patch_name} is avaiable!`);
+
+          client.guilds.cache.forEach((guild) => {
+            const role = guild.roles.cache.find(
+              (r) =>
+                r.name.toLowerCase().includes(message.role_name.toLocaleLowerCase()) &&
+                !r.name.includes('Dota2 Patches')
+            );
+            let msg = role ? `${role}` : '';
+            Object.keys(pnote)
+              .filter((key) => {
+                return (
+                  key !== 'patch_number' &&
+                  key !== 'patch_name' &&
+                  key !== 'patch_timestamp' &&
+                  key !== 'success'
+                );
+              })
+              .forEach((category) => {
+                const changes = pnote[category];
+                const categoryName = category.charAt(0).toUpperCase() + category.slice(1);
+                msg += `\n**${categoryName}**\n`;
+                switch (category) {
+                  case 'generic':
+                    changes.forEach((change) => (msg += `- ${change.note}\n`));
+                    break;
+
+                  case 'items':
+                    changes.forEach((change) => {
+                      const id = change.ability_id;
+                      const item_name = items.find((i) => i.id == id).name_english_loc;
+                      const notes = change.ability_notes;
+                      msg += `**${item_name}**\n`;
+                      notes.forEach((note) => (msg += `- ${note.note}\n`));
+                    });
+                    break;
+
+                  case 'heroes':
+                    changes.forEach((change) => {
+                      const hero_id = change.hero_id;
+                      const hero_name = heroes.find((h) => h.id == hero_id).name_english_loc;
+                      const hero_notes = change.hero_notes;
+                      const talent_notes = change.talent_notes;
+                      const abilities = change.abilities;
+                      msg += `**${hero_name}**\n`;
+
+                      if (hero_notes) {
+                        hero_notes.forEach((i) => (msg += `- ${i.note}\n`));
+                      }
+
+                      if (talent_notes) {
+                        talent_notes.forEach((i) => (msg += `- ${i.note}\n`));
+                      }
+
+                      if (abilities) {
+                        abilities.forEach((i) =>
+                          i.ability_notes.forEach((note) => (msg += `- ${note.note}\n`))
+                        );
+                      }
+                    });
+                    break;
+
+                  case 'neutral_items':
+                    changes.forEach((change) => {
+                      const id = change.ability_id;
+                      const item_name = items.find((i) => i.id == id).name_english_loc;
+                      const notes = change.ability_notes;
+                      msg += `**${item_name}**\n`;
+                      notes.forEach((note) => (msg += `- ${note.note}\n`));
+                    });
+                    break;
+                }
+              });
+
+            const embed = {
+              color: 0x0099ff,
+              title: `Patch ${pnote.patch_name}`,
+              url: `https://www.dota2.com/patches/${pnote.patch_name}`,
+              description: 'There is a new patch!',
+              thumbnail: {
+                url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_symbol.png',
+              },
+              fields: [
+                {
+                  name: 'Patch',
+                  value: pnote.patch_name,
+                  inline: true,
+                },
+                {
+                  name: 'Patch notes',
+                  value: `[Here](https://www.dota2.com/patches/${pnote.patch_name})`,
+                  inline: true,
+                },
+              ],
+            };
+
+            // Send update notification
+            const updateChannel = guild.channels.cache.filter(
               (c) =>
                 c.isText &&
-                c.name.toLowerCase().includes(message.channel_changelog_name.toLocaleLowerCase())
+                c.name.toLowerCase().includes(message.channel_update_name.toLocaleLowerCase())
             );
-            if (changelogChannels) {
-              changelogChannels.forEach((ch) =>
-                ch.send(splittedMessage).catch((err) => helper.error(err))
-              );
+            if (updateChannel) {
+              updateChannel.forEach((ch) => {
+                role
+                  ? ch
+                      .send({ content: `${role}`, embeds: [embed] })
+                      .catch((err) => helper.error(err))
+                  : ch.send({ embeds: [embed] }).catch((err) => helper.error(err));
+              });
             }
-          });
-        });
 
-        fs.writeFile(
-          './src/latest_patch.json',
-          JSON.stringify({ patch: pnote.patch_name }),
-          (err) => {
-            if (err) throw err;
-          }
-        );
+            // Send changelogs
+            Discord.Util.splitMessage(msg, { maxLength: 2000 }).forEach((splittedMessage) => {
+              const changelogChannels = guild.channels.cache.filter(
+                (c) =>
+                  c.isText &&
+                  c.name.toLowerCase().includes(message.channel_changelog_name.toLocaleLowerCase())
+              );
+              if (changelogChannels) {
+                changelogChannels.forEach((ch) =>
+                  ch.send(splittedMessage).catch((err) => helper.error(err))
+                );
+              }
+            });
+          });
+
+          fs.writeFile(
+            './src/latest_patch.json',
+            JSON.stringify({ patch: pnote.patch_name }),
+            (err) => {
+              if (err) throw err;
+            }
+          );
+        });
       });
     } catch (error) {
       helper.error(error);
