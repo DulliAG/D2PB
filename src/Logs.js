22d1c84b0ced37a05fad575221bb7a6ff79fd7f8
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-12-09 22:51:27 +0100

    :bug: Prevent bot-crashes due to database-timeouts

--- src/Logs.js
+++ src/Logs.js
@@ -1,16 +1,20 @@
 const { DatabaseCredentials, Logger } = require('@dulliag/logger.js');
 require('dotenv').config();
 
-const credentials = new DatabaseCredentials(
-  process.env.DB_HOST,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  process.env.DB_DATABASE
-);
+try {
+  const credentials = new DatabaseCredentials(
+    process.env.DB_HOST,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    process.env.DB_DATABASE
+  );
 
-const LOGGER = new Logger(credentials, 'Dota2PatchBot');
+  const LOGGER = new Logger(credentials, 'Dota2PatchBot');
 
-module.exports = {
-  credentials,
-  logger: LOGGER,
-};
+  module.exports = {
+    credentials,
+    logger: LOGGER,
+  };
+} catch (err) {
+  console.log(err);
+}
