f63b52924dd4279e6798eb39ebadca5084da76e4
Thorben <38816229+tklein1801@users.noreply.github.com>
2022-03-30 23:50:07 +0200

    :heavy_plus_sign: Update @dulliag/logger.js

--- src/Logs.js
+++ src/Logs.js
@@ -1,14 +1,14 @@
-const { Database, LogVariant, Credentials, Client } = require('@dulliag/logger.js');
+const { Database, LogVariant, Client } = require('@dulliag/logger.js');
 const helper = require('@dulliag/discord-helper');
 require('dotenv').config();
 
 try {
-  const credentials = new Credentials(
-    process.env.DB_HOST,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    process.env.DB_DATABASE
-  );
+  const credentials = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  };
 
   const LOGGER = new Client(Database.PG, credentials, 'Dota2PatchBot');
 
@@ -19,12 +19,6 @@ try {
    * @param {string} message
    */
   const createLog = (variant, code, message) => {
-    if (variant.ERROR || variant.WARNING) {
-      helper.error(message);
-    } else {
-      helper.log(message);
-    }
-
     if (process.env.PRODUCTION == 'true') {
       LOGGER.log(variant, code, message);
     }
